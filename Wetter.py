da0a1b352faa25fc6a582184202d94e211e07190
mazelger <jerremiezelger@gmail.com>
2025-05-20 14:31:02 +0200

    Update Wetter.py

--- Wetter.py
+++ Wetter.py
@@ -1,17 +1,13 @@
-import urllib.request
+import requests
 import json
 
 ort = "Innsbruck"
 url = f"https://wttr.in/{ort}?format=j1"
 
-# User-Agent setzen, damit JSON korrekt geliefert wird
-req = urllib.request.Request(
-    url,
-    headers={"User-Agent": "Mozilla/5.0"}
-)
-
-with urllib.request.urlopen(req) as response:
-    daten = json.loads(response.read().decode())
+# User-Agent setzen, damit JSON zurückkommt
+headers = {"User-Agent": "Mozilla/5.0"}
+response = requests.get(url, headers=headers)
+daten = json.loads(response.text)
 
 temperatur = daten["current_condition"][0]["temp_C"]
 print(f"In {ort} hat es aktuell {temperatur}°C")
